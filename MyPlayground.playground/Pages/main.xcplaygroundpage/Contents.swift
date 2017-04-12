71ff97b6ca20eed701c8c318e9b17b896246a18c
“Diljeet <jeetds1994@gmail.com>
2017-04-11 20:55:50 -0400

    fixed the funtions and left comments

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,13 @@
  */
 // write your code here
 
+func geo(continent: String, numberOfCountries: Int){
 
+    print("\(continent) is a continent which contatins \(numberOfCountries) countries")
+}
+
+geo(continent: "India", numberOfCountries: 5)
+//this is a example and is not factual
 
 
 /*: question2
@@ -25,8 +31,8 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
-
+greeting(name: "Danny", greeting: "Hello")
+// missing greeting: on the second arguement
 
 
 
@@ -37,8 +43,8 @@ func daysInMonth(month: String, days: String) {
     print("There are \(days) in \(month)")
 }
 
-daysInMonth(month: "November", days: 30)
-
+daysInMonth(month: "November", days: String(30))
+//Converted the days int 30 to String
 
 
 
@@ -47,7 +53,10 @@ daysInMonth(month: "November", days: 30)
  */
 // write your code here
 
+func threeParameters(beverage: String, numberOfBottles: Int, location: String){
 
-
-
+    print("\(String(numberOfBottles)) bottles of \(beverage) on the \(location) wall.")
+}
+threeParameters(beverage: "beer", numberOfBottles: 99, location: "shelf")
+// takes three arguements: two strings and one int
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
